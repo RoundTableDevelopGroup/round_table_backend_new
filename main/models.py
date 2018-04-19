e3bd52890ecf0f91e519a7f69bfe1c4459eb9c71
John Kindem <461425614@qq.com>
2018-04-19 18:25:16 +0800

    * add: knight_id生成器

--- main/models.py
+++ main/models.py
@@ -6,3 +6,36 @@ class Channel(models.Model):
     name = models.CharField(max_length=40)
     # 是否火
     hot = models.BooleanField(default=False)
+
+
+class User(models.Model):
+    # 用户名
+    username = models.CharField(max_length=32)
+    # 密码Hash
+    password = models.CharField(max_length=64)
+    # 盐
+    salt = models.CharField(max_length=12)
+    # 骑士id
+    knight_id = models.CharField(max_length=8)
+    # 管理员
+    admin = models.BooleanField(default=False)
+    # 头像
+    avatar = models.CharField(max_length=100, blank=True)
+    # 注册时间
+    register_time = models.DateTimeField(auto_now_add=True)
+    # 性别
+    sex = models.CharField(max_length=2, blank=True)
+    # 手机号
+    phone = models.CharField(max_length=20, blank=True)
+    # 邮箱
+    email = models.CharField(max_length=48, blank=True)
+    # 关注频道
+    channel = models.CharField(max_length=100, blank=True)
+    # QQ
+    qq = models.CharField(max_length=20, blank=True)
+    # 微博
+    weibo = models.CharField(max_length=20, blank=True)
+    # 微信
+    wechat = models.CharField(max_length=20, blank=True)
+    # steam
+    steam = models.CharField(max_length=20, blank=True)
---
+++ main/proxy/user.py
@@ -0,0 +1,34 @@
+from main.models import User
+import time
+import hashlib
+
+
+# 骑士号生成器类
+class KnightId:
+    __vertex = [
+        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+        'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+        'u', 'v', 'w', 'x', 'y', 'z'
+    ]
+
+    @classmethod
+    def generate(cls, username):
+        """
+        生成骑士id
+        :param username: 用户名
+        :return: 骑士id
+        """
+        # 将用户名和当前时间连接起来，构成骑士号种子
+        torrent = username + str(time.time())
+        # 获取种子的md5值
+        md5_hash = hashlib.md5(torrent.encode('utf-8')).hexdigest()
+        # 将MD5值每4位分组，求出4个数字的和并模36，并且经过骑士号转换矩阵构成骑士号的一位
+        knight_id = ''
+        tmp = 0
+        for i in range(len(md5_hash)):
+            tmp += int(md5_hash[i], 16)
+            if i % 4 == 3:
+                knight_id += cls.__vertex[tmp % 36]
+                tmp = 0
+        return knight_id
